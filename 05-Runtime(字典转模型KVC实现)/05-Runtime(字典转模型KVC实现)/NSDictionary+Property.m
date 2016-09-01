ceae72c030763381daf96132361f00ebb4df95e2
iChiaGuo <1026971792@qq.com>
2016-09-01 15:40:12 +0800

    Update NSDictionary+Property.m

--- 05-Runtime(字典转模型KVC实现)/05-Runtime(字典转模型KVC实现)/NSDictionary+Property.m
+++ 05-Runtime(字典转模型KVC实现)/05-Runtime(字典转模型KVC实现)/NSDictionary+Property.m
@@ -1,10 +1,3 @@
-//
-//  NSDictionary+Property.m
-//  05-Runtime(字典转模型KVC实现)
-//
-//  Created by xiaomage on 16/3/4.
-//  Copyright © 2016年 小码哥. All rights reserved.
-//
 
 #import "NSDictionary+Property.h"
 
